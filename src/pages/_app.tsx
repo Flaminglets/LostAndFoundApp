e386a52e6900050060a9b6f39117c2a0b16184b0
YoonseoJ <cassie961206@gmail.com>
2021-10-24 21:21:54 -0700

    header, footer, cards, responsive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css';
 import '../styles/loginregister.css';
 import '../styles/headerfooter.sass';
 import '../styles/homepage.sass';
+import '../styles/userpage.sass';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
