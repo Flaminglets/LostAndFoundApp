c2e96eee6e924dba97378135dc33f325a7486560
Nicole Tan <000431652@student.vcc.ca>
2021-11-08 21:41:03 -0800

    created post.tsx in api

---
+++ src/pages/api/post.tsx
@@ -0,0 +1,29 @@
+import { createAddPosts, getAddPosts } from '../../../lib/backend/database';
+
+//step 3, create the API
+export default async function handler(req, res) {
+    if (req.method == 'POST') {
+        const data = req.body;
+        const { type, date, time, location,
+            lostFname, lostLname, gender, otherGender,
+            age, weight, height, eyecolor, ethnicity,
+            additional, userFname, userLname, phoneNum,
+            email } = data; // destructuring
+
+        // step 4, connect to db and create cheetah
+        await createAddPosts(type, date, time, location,
+            lostFname, lostLname, gender, otherGender,
+            age, weight, height, eyecolor, ethnicity,
+            additional, userFname, userLname, phoneNum,
+            email);
+        res.status(200).json(
+            {
+                success : true
+            }
+        );
+        return;
+    }
+    const data = await getAddPosts();
+    res.status(200).json(data);
+}
+  
