3d61bb738a432b6da65de91bed821fe69c3cbba3
nicole <000431652@student.vcc.ca>
2021-11-23 16:11:47 -0800

    modified database

--- lib/backend/database.tsx
+++ lib/backend/database.tsx
@@ -27,5 +27,77 @@ export async function getPostByType(postType) {
     return post;
 }
 
+export async function getUser(user) {
+    // connect to the client
+    const client = await mongoose.connect(uri);
+    const users = await User.findOne(user);
 
+    return users;
+};
+
+
+export async function createUser(name, email, image) {
+    const client = await mongoose.connect(uri);
+    const user = new User(
+        {
+            name,
+            email,
+            image,
+        }
+    )
+
+    return user.save();
+}
+
+// Get multiple post 
+export async function getAddPosts() {
+    const client = mongoose.connect(uri);
+    const addposts = await Addpost.find()
+    return addposts;
+}
+
+// Find one post
+export async function getSinglePost(aPostID) {
+    const client = await mongoose.connect(uri);
+    const post = await Addpost.findById(aPostID);
+
+    return post;
+}
+
+// Create a post
+export async function createAddPosts(type, date, time, location, lostFname, lostLname, gender, otherGender, age, weight, height, eyecolor, additional, image, userFname, userLname, phoneNum, email, userID) {
+    const client = mongoose.connect(uri);
+    const addpost = await new Addpost(
+        {
+            type, 
+            date, 
+            time, 
+            location, 
+            lostFname, 
+            lostLname, 
+            gender, 
+            otherGender, 
+            age, 
+            weight, 
+            height, 
+            eyecolor, 
+            additional, 
+            image,
+            userFname, 
+            userLname, 
+            phoneNum, 
+            email, 
+            userID
+        }
+    )
+
+    addpost.user = userID;
+    await addpost.save();
+
+    const userByID = await User.findById(userID);
+    userByID.posts.push(addpost);
+    await userByID.save();
+
+    return;
+}
 
