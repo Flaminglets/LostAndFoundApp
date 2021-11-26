66173e060b2717c149b46f3a6e521fd2f18b9bb9
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2021-11-25 21:45:17 -0800

    Add files via upload

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -17,6 +17,7 @@ export default function PopUp(props) {
         window.print();
         document.body.innerHTML = originalContent;
     }
+    console.log(props.data);
     return (
         <div id="missing-card" className={styles.boxwrapper}>
             <div className={styles.box}>
@@ -29,41 +30,63 @@ export default function PopUp(props) {
                     </div>
                 </div>
                   
-                <h1 className={styles.red}>MISSING</h1>
-                <img className={styles.img} alt="Not Avail" src={props.data.image}/>
                 <h3 className={styles.name}>{props.data.lostFname} {props.data.lostLname}</h3>
+                <img className={styles.img} alt="Not Avail" src={props.data.image}/>
                 <div className={styles.grid}>
                     <div className={styles.left}>
                         <ul className="list">
                             <li className={styles.listitem}>
-                                <b>Gender:</b> {props.data.gender ? `${props.data.gender}` : "N/A"}
+                                Gender: <b>{props.data.gender ? `${props.data.gender}` : "N/A"}</b>
                                
                             </li>
                             <li className={styles.listitem}>
-                                <b>Date of Birth:</b> {props.data.dateOfBirth ? `${props.data.dateOfBirth}` : "N/A"}
+                                Date of Birth: <b>{props.data.dateOfBirth ? `${props.data.dateOfBirth}` : "N/A"}</b>
                             </li>
                             <li className={styles.listitem}>
-                                <b>Height:</b> {props.data.strHeight ? `${props.data.strHeight}` : props.data.height ? `${props.data.height}` : " "} 
+                                Height: <b>{props.data.strHeight ? `${props.data.strHeight}` : props.data.height ? `${props.data.height}` : " "} </b>
                             </li>
                             <li className={styles.listitem}>
-                                <b>Weight:</b> {props.data.strWeight ? `${props.data.strWeight}` : props.data.weight ? `${props.data.weight}` : " "}
+                                Weight: <b>{props.data.strWeight ? `${props.data.strWeight}` : props.data.weight ? `${props.data.weight}` : " "}</b>
                             </li>
                             <li className={styles.listitem}>
-                                <b>Eye Color:</b> {props.data.eyecolor ? `${props.data.eyecolor}` : "N/A"}
+                                Eye Color: <b>{props.data.eyecolor ? `${props.data.eyecolor}` : "N/A"}</b>
                             </li>
                             <li className={styles.listitem}>
-                                <b>Hair Color:</b> {props.data.haircolor ? `${props.data.haircolor}` : "N/A"}
+                                Hair Color: <b>{props.data.haircolor ? `${props.data.haircolor}` : "N/A"}</b>
                             </li>
                         </ul>
                     </div>
                     <div className={styles.right}>
                     <ul className="list">
                             <li className={styles.listitem}>
-                                <b>Last Seen:</b> {props.data.location ? `${props.data.location} ` : "N/A"}{props.data.date ? `, ${props.data.date}` : ""}
+                                Last Seen:
                             </li>
+                            <li className={styles.listitem}>
+                                - Location: <b>{props.data.location}</b>
+                            </li>
+                            <li className={styles.listitem}>
+                                - Date: <b>{props.data.date ? props.data.date : "N/A"}</b>
+                            </li>
+                            <li className={styles.listitem}>
+                                - Time: <b>{props.data.time ? props.data.time : "N/A"}</b>
+                            </li>
+                            <li className={styles.listitem}>
+                                Contact Info:
+                            </li>
+                            <li className={styles.listitem}>
+                                - Phone Num: <b>{props.data.phoneNum ? props.data.phoneNum : "N/A"}</b>
+                            </li>
+                            <li className={styles.listitem}>
+                                - Email: <b>{props.data.email ? props.data.email : "N/A"}</b>
+                            </li>   
                         </ul>
                     </div>
                 </div>
+                <div className={styles.additionalInfoWrap}>
+                   <h4>Additional Information: </h4> 
+                   <p>{props.data.additional}</p>
+                </div>
+                
             </div>
         </div>
     )
