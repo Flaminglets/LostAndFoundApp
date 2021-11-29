8f80728ff8008567228e2ae1de28a64597eaf0fc
dannydeng <dinghaoden1@gmail.com>
2021-11-28 21:13:45 -0800

    adjust login to a div

--- src/pages/auth/loginregister.tsx
+++ src/pages/auth/loginregister.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { getProviders, signIn, getSession, getCsrfToken } from "next-auth/client";
+import { getProviders, signIn, getSession } from "next-auth/client";
 
 // By Danny @Flaminglets
 // Sign in page for users
@@ -13,23 +13,18 @@ export default function LoginRegister({ providers }) {
         <div>
             <div className={`L_container ${event}`} id="container">
                 <div className="form-container sign-in-container">
-                    <form className="login-signup-form">
+                    <div className="login-signup-form">
                         <h1 className="h1-title">Login/Register</h1>
                         {Object.values(providers).map((provider: any) => {
                             return (
                                 <div key={provider.name}>
-                                    <button className="provider-button" onClick={
-                                        () => {
-                                            signIn(provider.id)
-                                            console.log(provider.id)
-                                        }
-                                    }>
+                                    <button className="provider-button" onClick={() => signIn(provider.id)}>
                                         Sign in with {provider.name}
                                     </button>
                                 </div>
                             );
                         })}
-                    </form>
+                    </div>
                 </div>
                 <div className="overlay-container">
                     <div className="overlay">
@@ -49,10 +44,6 @@ export async function getServerSideProps(context) {
     const { req, res } = context;
     const session = await getSession({ req });
     const providers = await getProviders();
-    const csrfToken = await getCsrfToken(context);
-
-    console.log("providers", providers);
-    console.log("csrfToken", csrfToken);
 
     if (session && res) {
         res.writeHead(302, {
@@ -65,8 +56,9 @@ export async function getServerSideProps(context) {
     return {
         props: {
             providers,
-            csrfToken,
             session: null,
         }
     };
 };
+
+
